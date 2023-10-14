cdcea236b5a2cdafe92b901fce5007cd16de3f1a
inoue62 <132170302+inoue62@users.noreply.github.com>
2023-10-14 11:46:34 +0900

    https対応

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,5 +20,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if(\App::environment(['production'])){
+            \URL::forceScheme('https');
+            $this->app['request']->server->set('HTTPS','on');
+        }
+        
+        Paginator::useBootstrap();
     }
 }
